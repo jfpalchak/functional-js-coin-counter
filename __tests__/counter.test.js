851e61e746d93f38cd33ec9a44d832aaddaaab81
Richard Barbour <richyb07@gmail.com>
2023-11-06 14:11:32 -0800

    Add Jest tests with full coverage.
    Co-authored-by: Joey Palchak <jfpalchak@users.noreply.github.com>
    Co-authored-by: Jon Cheng <joncheng-dev@users.noreply.github.com>

--- __tests__/counter.test.js
+++ __tests__/counter.test.js
@@ -1,2 +1,54 @@
-import { SampleInputFunc } from './../src/js/counter.js';
+import { ChangeCalculator } from './../src/js/counter.js';
 
+describe("ChangeCalculator", () => {
+  test("should take in dollarAmount valued at 0 as an argument and return an empty string", () => {
+    // Arrange
+    const dollarAmount = 0;
+    // Act
+    const stringResult = ChangeCalculator(dollarAmount);
+    // Assert
+    expect(stringResult).toEqual("");
+  });
+
+  test("should take in dollarAmount valued at .04 as an argument and return 4 pennies.", () => {
+    const dollarAmount = .04;
+    const stringResult = ChangeCalculator(dollarAmount);
+    expect(stringResult).toEqual("Pennies: 4, ");
+  });
+
+  test("should take in dollarAmount valued at .05 as an argument and return 1 nickel.", () => {
+    const dollarAmount = .05;
+    const stringResult = ChangeCalculator(dollarAmount);
+    expect(stringResult).toEqual("Nickels: 1, ");
+  });
+
+  test("should take in dollarAmount valued at .08 as an argument and return 1 nickel, 3 pennies.", () => {
+    const dollarAmount = .08;
+    const stringResult = ChangeCalculator(dollarAmount);
+    expect(stringResult).toEqual("Pennies: 3, Nickels: 1, ");
+  });
+
+  test("should take in dollarAmount valued at .10 as an argument and return 1 dime.", () => {
+    const dollarAmount = .1;
+    const stringResult = ChangeCalculator(dollarAmount);
+    expect(stringResult).toEqual("Dimes: 1, ");
+  });
+
+  test("should take in dollarAmount valued at .16 as an argument and return 1 dime, 1 nickel, and 1 penny.", () => {
+    const dollarAmount = .16;
+    const stringResult = ChangeCalculator(dollarAmount);
+    expect(stringResult).toEqual("Pennies: 1, Nickels: 1, Dimes: 1, ");
+  });
+
+  test("should take in dollarAmount valued at .25 as an argument and return 1 quarter.", () => {
+    const dollarAmount = .25;
+    const stringResult = ChangeCalculator(dollarAmount);
+    expect(stringResult).toEqual("Quarters: 1.");
+  });
+  
+  test("should take in dollarAmount valued at 1.41 as an argument and return 5 quarters, 1 dime, 1 nickel, and 1 penny.", () => {
+    const dollarAmount = 1.41;
+    const stringResult = ChangeCalculator(dollarAmount);
+    expect(stringResult).toEqual("Pennies: 1, Nickels: 1, Dimes: 1, Quarters: 5.");
+  });
+});
--- src/js/counter.js
+++ src/js/counter.js
@@ -1,35 +1,33 @@
-function SampleInputFunc(value) {
+export function ChangeCalculator(value) {
   if (value <= 0) {
-    return "";
-  }
-  if (value === 0) {
     return "";
   } else {
 
     if (value >= .25) {
       const resultA = Math.floor(value / .25);
       const resultB = (value % .25).toFixed(2);
-      return SampleInputFunc(resultB) + `Quarters: ${resultA},`;
+      return ChangeCalculator(resultB) + `Quarters: ${resultA}.`;
     } else if (value >= .10) {
       const resultA = Math.floor(value / .1);
       const resultB = (value % .1).toFixed(2);
-      return SampleInputFunc(resultB) + `Dimes: ${resultA}, `;
+      return ChangeCalculator(resultB) + `Dimes: ${resultA}, `;
     } else if (value >= .05) {
       const resultA = Math.floor(value / .05);
       const resultB = (value % .05).toFixed(2);
       // console.log(`ResultA at Nickels: ${resultA}`);
       // console.log(`ResultB at Nickels: ${resultB}`);
-      return SampleInputFunc(resultB) + `Nickels: ${resultA}, `;
-    } else if (value >= .01) {
+      return ChangeCalculator(resultB) + `Nickels: ${resultA}, `;
+    } else {
+      console.log(`value: ${value}`);
       const resultA = Math.floor(value / .01);
-      const resultB = (value % .01).toFixed(2);
-      // console.log(`ResultA at Pennies: ${resultA}`);
-      // console.log(`ResultB at Pennies: ${resultB}`);
-      return SampleInputFunc(resultB) + `Pennies: ${resultA}, `;
+      const resultB = (value % .01).toFixed(1);
+      console.log(`ResultA at Pennies: ${resultA}`);
+      console.log(`ResultB at Pennies: ${resultB}`);
+      return ChangeCalculator(resultB) + `Pennies: ${resultA}, `;
     }
   }
 }
 
-console.log(SampleInputFunc(.17));
+// console.log(ChangeCalculator(.27));
 
 // numArray.push(resultA);
