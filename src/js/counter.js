b0006e8470e97afa502d938f130f18b453307e8a
Richard Barbour <richyb07@gmail.com>
2023-11-06 11:40:49 -0800

    Return results of recursive function as a string.
    Co-authored-by: Joey Palchak <joey.palchak@gmail.com>
    Co-authored-by: Jon Cheng <joncheng.dev@gmail.com>

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,6 +1,6 @@
 function SampleInputFunc(value) {
   if (value <= 0) {
-    return;
+    return "";
   }
   if (value === 0) {
     return "";
@@ -8,24 +8,28 @@ function SampleInputFunc(value) {
 
     if (value >= .25) {
       const resultA = Math.floor(value / .25);
-      const resultB = value % .25;
-      return SampleInputFunc(resultB) + resultA;
+      const resultB = (value % .25).toFixed(2);
+      return SampleInputFunc(resultB) + `Quarters: ${resultA},`;
     } else if (value >= .10) {
       const resultA = Math.floor(value / .1);
-      const resultB = value % .1;
-      return SampleInputFunc(resultB) + resultA;
+      const resultB = (value % .1).toFixed(2);
+      return SampleInputFunc(resultB) + `Dimes: ${resultA}, `;
     } else if (value >= .05) {
       const resultA = Math.floor(value / .05);
-      const resultB = value % .05;
-      return SampleInputFunc(resultB) + resultA;
+      const resultB = (value % .05).toFixed(2);
+      console.log(`ResultA at Nickels: ${resultA}`);
+      console.log(`ResultB at Nickels: ${resultB}`);
+      return SampleInputFunc(resultB) + `Nickels: ${resultA}, `;
     } else if (value >= .01) {
       const resultA = Math.floor(value / .01);
-      const resultB = value % .01;
-      return SampleInputFunc(resultB) + resultA;
+      const resultB = (value % .01).toFixed(2);
+      console.log(`ResultA at Pennies: ${resultA}`);
+      console.log(`ResultB at Pennies: ${resultB}`);
+      return SampleInputFunc(resultB) + `Pennies: ${resultA}, `;
     }
   }
 }
 
-SampleInputFunc(1.41);
+console.log(SampleInputFunc(.17));
 
 // numArray.push(resultA);
